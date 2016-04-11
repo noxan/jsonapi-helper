4157649e096bff347544ad9d6a63fa044d579827
Richard <hello@noxan.net>
2016-04-11 14:23:06 +0200

    add more advanced test case for complete serialization

---
+++ test/complete.js
@@ -0,0 +1,30 @@
+import test from 'ava';
+
+import Schema from '../src';
+
+
+test('serialize complete basic object', async t => {
+  const obj = {
+    ID: '5',
+    NaMe: 'Hello',
+  };
+
+  const objSchema = new Schema({
+    id: 'ID',
+    type: 'objs',
+    attributes: {
+      name: 'NaMe',
+    }
+  });
+
+  const result = objSchema.serialize(obj);
+
+  t.deepEqual(result, {
+    links: {},
+    data: {
+      type: 'objs',
+      id: '5',
+      attributes: { name: 'Hello' }
+    },
+  });
+});
