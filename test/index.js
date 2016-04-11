36d10849d5fd9312dfbb9083a47ef2f43daf333f
Richard <hello@noxan.net>
2016-04-11 12:21:06 +0200

    serialize should return obj

--- test/index.js
+++ test/index.js
@@ -8,3 +8,11 @@ test('schema object has serialize method', async t => {
 
   t.truthy(schema.serialize);
 });
+
+test('schema serialize returns an object', async t => {
+  const schema = new Schema();
+  const result = schema.serialize();
+
+  t.truthy(result);
+  t.is(typeof result, "object");
+});
