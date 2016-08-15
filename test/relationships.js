43611b84e29760c9388991dacd4eeca15c0e42ea
Richard <hello@noxan.net>
2016-08-15 12:35:32 +0200

    restructure relationship tests and extract schemas from test case

--- test/relationships.js
+++ test/relationships.js
@@ -2,6 +2,31 @@ import test from 'ava';
 
 import Schema from '../src';
 
+const objSchema = new Schema({
+  type: 'objs',
+  relationships: {
+    elements: {
+      type: 'somethings',
+      id: el => el.name,
+    }
+  },
+});
+
+const objSchema2 = new Schema({
+  id: 'ID',
+  type: 'objs',
+  attributes: {
+    name: 'NaMe',
+  },
+  relationships: {
+    mountains: {
+      type: 'mountains',
+      id: el => el,
+    }
+  },
+});
+
+
 test('serialize complete object with relationship', async t => {
   const obj = {
     ID: '5',
@@ -9,21 +34,7 @@ test('serialize complete object with relationship', async t => {
     mountains: [3, 7],
   };
 
-  const objSchema = new Schema({
-    id: 'ID',
-    type: 'objs',
-    attributes: {
-      name: 'NaMe',
-    },
-    relationships: {
-      mountains: {
-        type: 'mountains',
-        id: el => el,
-      }
-    },
-  });
-
-  const result = objSchema.serialize(obj);
+  const result = objSchema2.serialize(obj);
 
   t.deepEqual(result, {
     links: {},
@@ -52,16 +63,6 @@ test('serialize relationship with id resolve function', async t => {
     ],
   };
 
-  const objSchema = new Schema({
-    type: 'objs',
-    relationships: {
-      elements: {
-        type: 'somethings',
-        id: el => el.name,
-      }
-    },
-  });
-
   const result = objSchema.serialize(obj);
 
   t.deepEqual(result.data.relationships.elements.data, [
@@ -78,20 +79,7 @@ test('serialize relationship with single object', async t => {
     ],
   };
 
-  const objSchema = new Schema({
-    type: 'objs',
-    relationships: {
-      elements: {
-        type: 'somethings',
-        id: el => el.name,
-      }
-    },
-  });
-
   const result = objSchema.serialize(obj);
 
-  t.deepEqual(
-    result.data.relationships.elements.data,
-    { type: 'somethings', id: 'tdcaw' }
-  );
+  t.deepEqual(result.data.relationships.elements.data, { type: 'somethings', id: 'tdcaw' });
 });
